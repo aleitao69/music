b246cb6ec0813f75b0751d1403d978b16d7bd7c2
Danivalois <valois.daniela@gmail.com>
2025-06-20 10:30:46 -0300

    Adjustments

--- audio/views.py
+++ audio/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 def flac_player_view(request):
     # These URLs could be local/static or remote
     flac_files = [
-        '/media/audio/200moremiles.flac',
+        '/media/audio/hob.mp3',
     
     ]
     return render(request, 'audio/player.html', {'flac_files': flac_files})
