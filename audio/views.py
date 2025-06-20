3f452442808a950e0104f53e3197eb86de89a7e3
Danivalois <valois.daniela@gmail.com>
2025-06-20 11:45:21 -0300

    🛠️ Auto-commit

--- audio/views.py
+++ audio/views.py
@@ -1,10 +1,12 @@
+import json
 from django.shortcuts import render
-def flac_player_view(request):
-    # These URLs could be local/static or remote
-    flac_files = [
-        '/media/audio/200moremiles.flac',
-        '/media/audio/hob.mp3',
-    
-    ]
-    return render(request, 'audio/player.html', {'flac_files': flac_files})
+from django.conf import settings
+import os
 
+def flac_player_view(request):
+    audio_folder = os.path.join(settings.MEDIA_ROOT, 'audio')
+    flac_files = [f for f in os.listdir(audio_folder) if f.endswith(('.flac', '.mp3'))]
+    flac_urls = [f"/media/audio/{file}" for file in flac_files]
+    return render(request, 'flac_player.html', {
+        'flac_urls': json.dumps(flac_urls)  # 🔥 necessário para JSON no template
+    })
---
+++ commit.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "🛠️ Auto-commit"
+git push origin main
