f721f2a606ddd052b313d805deecbf44ae095a98
Danivalois <valois.daniela@gmail.com>
2025-06-20 14:26:04 -0300

    🛠️ Auto-commit

--- audio/views.py
+++ audio/views.py
@@ -3,8 +3,17 @@ from django.shortcuts import render
 from django.conf import settings
 import os
 
+
 def flac_player_view(request):
-    audio_dir = os.path.join(settings.MEDIA_ROOT, "audio")
-    files = os.listdir(audio_dir)
-    flac_files = [f"/media/audio/{f}" for f in files if f.lower().endswith((".flac", ".mp3"))]
-    return render(request, "flac_player.html", {"flac_files": flac_files})
+    audio_folder = os.path.join('media', 'audio')
+    audio_files = []
+
+    for file in os.listdir(audio_folder):
+        if file.endswith(('.flac', '.mp3')):
+            audio_files.append(f"/media/audio/{file}")
+
+    context = {
+        "flac_files": audio_files
+    }
+
+    return render(request, "flac_player.html", context)
